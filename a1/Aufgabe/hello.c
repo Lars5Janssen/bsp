9435a3d4f270fe5fb353518f7007508e6fa8c637
Elias Wernicke <e.w.1111111119@gmail.com>
2023-11-03 15:11:38 +0100

    init A1

---
+++ a1/Aufgabe/hello.c
@@ -0,0 +1,14 @@
+/*
+* <Description of this C program>
+*
+* <Your name> # <Date>
+*
+* Compile: gcc -o hello hello.c
+*/
+
+#include <stdio.h>
+
+int main () {
+	printf("Willkommen im BSP!\n");
+	return 0;
+}

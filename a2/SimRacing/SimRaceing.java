960ad479f8658dee65bd636ed6bc3a42352660e6
lars <lars.janssen@haw-hamburg.de>
2023-12-03 15:00:03 +0100

    More interestin values

--- a2/SimRacing/SimRaceing.java
+++ a2/SimRacing/SimRaceing.java
@@ -1,12 +1,12 @@
 package SimRacing;
 
 public class SimRaceing {
-    private static Integer numberOfCars = 4;
-    private static Integer numberOfLaps = 1;
 
     public static void main(String[] args) {
+        int numberOfCars = 5;
         Monitor monitor = new Monitor(numberOfCars);
         for (int i = 0; i < numberOfCars; i++) {
+            Integer numberOfLaps = 3;
             RaceCar raceCar = new RaceCar(i, numberOfLaps, monitor);
             raceCar.start();
         }
